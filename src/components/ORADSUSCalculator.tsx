19d6d0d79e89364099a5e68f8d2e15c3b63ca5d3
atuls-orads <atul.shinagare@gmail.com>
2026-04-28 19:46:16 -0400

    Create ORADSUSCalculator component

---
+++ src/components/ORADSUSCalculator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ORADSUSCalculator: React.FC = () => {
+    return (
+        <div>
+            <h1>ORADS US Calculator</h1>
+            {/* Add your calculator logic here */}
+        </div>
+    );
+};
+
+export default ORADSUSCalculator;
