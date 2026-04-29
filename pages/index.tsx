3b09c567739b912d84fb66cfcb785693bc72a23c
atuls-orads <atul.shinagare@gmail.com>
2026-04-28 20:10:21 -0400

    Created pages/index.tsx to import ORADSUSCalculator and export Home component

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import ORADSUSCalculator from '../components/ORADSUSCalculator';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to ORADS US Calculator</h1>
+            <ORADSUSCalculator />
+        </div>
+    );
+};
+
+export default Home;
